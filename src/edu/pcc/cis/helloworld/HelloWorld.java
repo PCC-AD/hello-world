cabfcf1144cd47893105ef70baaf39864808f0b0
Anteneh <anteneh.demissie@pcc.edu>
2023-02-07 23:28:32 -0800

    Complete Project

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,12 +1,28 @@
 package edu.pcc.cis.helloworld;
 /**
-    * Says Hello World.
-    *
-    * @author Anteneh Demissie
-    * @version 2023.02.07
+ * Says Hello World.
+ *
+ * @author Anteneh Demissie
+ * @version 2023.02.07
  */
 public class HelloWorld {
 
+    /**
+     * Say hello to the world
+     */
+    public void sayHello()
+    {
+        System.out.println("Hello, World!");
+    }
+
+    /**
+     * Say hello to a person by name
+     * !@param name
+     */
+    public void sayHello(String name)
+    {
+        System.out.println("Hello, " + name + "!");
+    }
 }
 
 
