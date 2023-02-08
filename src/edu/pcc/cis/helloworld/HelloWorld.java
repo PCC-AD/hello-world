88b20c614722cee9efddd2314380dd28447ba5e3
Anteneh <anteneh.demissie@pcc.edu>
2023-02-07 23:40:45 -0800

    added main method to print hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -10,19 +10,10 @@ public class HelloWorld {
     /**
      * Say hello to the world
      */
-    public void sayHello()
-    {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 
-    /**
-     * Say hello to a person by name
-     * !@param name
-     */
-    public void sayHello(String name)
-    {
-        System.out.println("Hello, " + name + "!");
-    }
 }
 
 
